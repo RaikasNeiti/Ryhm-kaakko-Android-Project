b73b369f8caba825d45d5d22bcb0ccdccd3c7dbd
RaikasNeiti <74050581+RaikasNeiti@users.noreply.github.com>
2020-11-27 13:08:30 +0200

    StepCounter

--- app/src/main/java/com/example/ryhmakaakkoapplication/MainActivity.java
+++ app/src/main/java/com/example/ryhmakaakkoapplication/MainActivity.java
@@ -2,13 +2,103 @@ package com.example.ryhmakaakkoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+    private TextView textView;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private static final int RESET = 0;
+    private int numSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //testitestistesti
+        setContentView(R.layout.activity_main);
+
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+        TextView Steps = findViewById(R.id.tv_steps);
+        Button BtnStart = findViewById(R.id.btn_start);
+        Button BtnStop = findViewById(R.id.btn_stop);
+        Button BtnSave = findViewById(R.id.btn_save);
+        Steps.setText(TEXT_NUM_STEPS + RESET);
+
+        //startButton
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+
+                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Steps.setText(TEXT_NUM_STEPS + RESET);
+                sensorManager.unregisterListener(MainActivity.this);
+                numSteps = 0;
+
+            }
+        });
+
+        BtnSave.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Steps.setText("Steps Saved");
+
+                sensorManager.unregisterListener(MainActivity.this);
+                numSteps = 0;
+
+            }
+        });
+
+
+
     }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        TextView Steps = findViewById(R.id.tv_steps);
+        numSteps++;
+        Steps.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
 }
